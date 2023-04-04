1cfc84f5758933f865167e806163c929184f10cb
diffloc <6632742+diffloc@users.noreply.github.com>
2023-04-04 11:06:49 -0500

    Stage 2/7 complete

--- Main.java
+++ Main.java
@@ -1,23 +1,35 @@
 package bullscows;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        int turn = 0;
-        int code = 1234;
-        System.out.println("The secret code is prepared: ****.");
+        String secretCode = "9305";
+        String userGuess = scanner.nextLine();
 
-        turn++;
-        int guess = 5678;
-        System.out.printf("\nTurn %d Answer:\n", turn);
-        System.out.println(guess);
-        System.out.println("Grade: None.");
+        int countBulls = 0;
+        int countCows = 0;
 
-        turn++;
-        guess = 1234;
-        System.out.printf("\nTurn %d Answer:\n", turn);
-        System.out.println(guess);
-        System.out.println("Grade: 4 bulls.");
-        System.out.printf("Congrats! The secret code is %d", code);
+        for (int i = 0; i < secretCode.length(); i++) {
+            if (userGuess.charAt(i) == secretCode.charAt(i)) {
+                countBulls++;
+            }
+
+            for (int j = 0; j < userGuess.length(); j++) {
+                if (userGuess.charAt(j) == secretCode.charAt(i) && j != i)
+                    countCows++;
+            }
+        }
+        String grade = String.format(
+                countBulls == 0 && countCows == 0 ? "None." :
+                        countBulls == 0 ? "%d cow(s)." :
+                                countCows == 0 ? "%d bull(s)." :
+                                        "%d bull(s) and %d cow(s).",
+                countBulls, countCows);
+        System.out.printf("Grade: %s The secret code is %s.", grade, secretCode);
+
+
     }
 }
